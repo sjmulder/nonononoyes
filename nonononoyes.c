026b404c46e8fac7f55e4943c7be3ec8fefa5a31
Sijmen J. Mulder <ik@sjmulder.nl>
2017-10-13 00:29:35 +0200

    Fix code style

--- nonononoyes.c
+++ nonononoyes.c
@@ -3,23 +3,22 @@
 
 int main(int argc, char **argv)
 {
-  char *nm, *p;
+	char *nm, *p;
 
-  p = nm = basename(argv[0]);  
-  while (1)
-    {
-      if (*p == 'y' && *(++p) == 'e' && *(++p) == 's')
-        puts("y");
-      else if (*p == 'n' && *(++p) == 'o')
-        puts("n");
-      else
-        goto invalid;
+	p = nm = basename(argv[0]);	
+	while (1) {
+		if (*p == 'y' && *(++p) == 'e' && *(++p) == 's')
+			puts("y");
+		else if (*p == 'n' && *(++p) == 'o')
+			puts("n");
+		else
+			goto invalid;
 
-      if (!*(++p))
-        p = nm;
-    }
+		if (!*(++p))
+			p = nm;
+	}
 
 invalid:
-  fprintf(stderr, "invalid program name\n");
-  return 1;
+	fprintf(stderr, "invalid program name\n");
+	return 1;
 }
